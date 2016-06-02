f7682382200fec74b8e8d7344e2e67a345da4468
degrs <gf dfggg>
2016-06-02 05:56:31 -0500

    Crono y tiempo que no funcionan

--- app/src/main/java/com/picking/pick2light/Crono.java
+++ app/src/main/java/com/picking/pick2light/Crono.java
@@ -13,46 +13,110 @@ import android.widget.TextView;
 import java.net.Socket;
 
 public class Crono extends AppCompatActivity implements View.OnClickListener {
-    Button startChrono;
+    /*Button startChrono;
     Button stopChrono;
     Button resetChrono;
-    Chronometer Chrono;
+    Chronometer Chrono;*/
     TextView titulo;
     long time=0;
     int est = 1;
+    Tiempo temp;
+    String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crono2);
+        /*setContentView(R.layout.activity_crono2);
         startChrono=(Button)findViewById(R.id.start);
         stopChrono=(Button)findViewById(R.id.stop);
         resetChrono=(Button)findViewById(R.id.reset);
         Chrono=(Chronometer)findViewById(R.id.chronometer);
         startChrono.setOnClickListener(this);
         stopChrono.setOnClickListener(this);
-        resetChrono.setOnClickListener(this);
-        titulo = (TextView)findViewById(R.id.est8);
-        Bundle bundle = getIntent().getExtras();
+        resetChrono.setOnClickListener(this);*/
+        titulo = (TextView)findViewById(R.id.textito);
 
-        Thread t = new Thread(){
+        temp = new Tiempo();
+        Thread t = new Thread() {
             @Override
             public void run() {
+                temp.Contar();
+            }
+        };
+        t.start();
+        /*while(true){
+            titulo.setText(temp.getSegundos());
+        }*/
+
+        //message = DBConnection.getInstance().receive();
+
+        /*Thread t = new Thread(){
+            @Override
+            public void run() {
+                TextView campo;
+                long tiempo;
+                int minutos, segundos;
+                String string_min, string_seg;
                 while (true) {
                     try {
                         //DBConnection.getInstance().send("2");
-                        String message = DBConnection.getInstance().receive();
-                        System.out.println(message);
-                        showToast("Estación: " + message);
-
+                        //String message = DBConnection.getInstance().receive();
+                        if (message.equals("12")) {
+                            est++;
+                            Chrono.setBase(SystemClock.elapsedRealtime() + time);
+                            Chrono.start();
+                            showToast("Estación: " + est);
+                        }
+                        if (message.equals("11")) {
+                            switch (est) {
+                                case 1:
+                                    campo = (TextView)findViewById(R.id.est1);
+                                    break;
+                                case 2:
+                                    campo = (TextView)findViewById(R.id.est2);
+                                    break;
+                                case 3:
+                                    campo = (TextView)findViewById(R.id.est3);
+                                    break;
+                                case 4:
+                                    campo = (TextView)findViewById(R.id.est4);
+                                    break;
+                                case 5:
+                                    campo = (TextView)findViewById(R.id.est5);
+                                    break;
+                                case 6:
+                                    campo = (TextView)findViewById(R.id.est6);
+                                    break;
+                                case 7:
+                                    campo = (TextView)findViewById(R.id.est7);
+                                    break;
+                                default:
+                                    campo = null;
+                                    break;
+                            }
+                            tiempo = Math.abs((Chrono.getBase()-SystemClock.elapsedRealtime())/1000);
+                            minutos = (int)(tiempo/60);
+                            segundos = (int)(tiempo-minutos*60);
+                            string_min = String.valueOf(minutos);
+                            string_seg = String.valueOf(segundos);
+                            if (minutos < 10) {string_min = "0"+string_min;}
+                            if (segundos < 10) {string_seg = "0"+string_seg;}
+                            campo.setText(string_min+":"+string_seg);
+                            Chrono.setBase(SystemClock.elapsedRealtime());
+                            time = 0;
+                            est++;
+                            if (est > 7){ est = 1; }
+                        }
                     }catch(Exception e){
                         e.printStackTrace();
                     }
                 }
             }
         };
-        t.start();
+        t.start();*/
+
     }
+
 
     public void showToast(final String toast)
     {
@@ -66,14 +130,10 @@ public class Crono extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        TextView campo;
-        long tiempo;
-        int minutos, segundos;
-        String string_min, string_seg;
+        /*
         switch (v.getId()) {
             case R.id.start:
-                Chrono.setBase(SystemClock.elapsedRealtime() + time);
-                Chrono.start();
+
                 break;
             case R.id.stop:
                 time = Chrono.getBase() + SystemClock.elapsedRealtime();
@@ -82,48 +142,11 @@ public class Crono extends AppCompatActivity implements View.OnClickListener {
             case R.id.reset:
 
                 //time = Chrono.getBase() + SystemClock.elapsedRealtime();
-                switch (est) {
-                    case 1:
-                        campo = (TextView)findViewById(R.id.est1);
-                        break;
-                    case 2:
-                        campo = (TextView)findViewById(R.id.est2);
-                        break;
-                    case 3:
-                        campo = (TextView)findViewById(R.id.est3);
-                        break;
-                    case 4:
-                        campo = (TextView)findViewById(R.id.est4);
-                        break;
-                    case 5:
-                        campo = (TextView)findViewById(R.id.est5);
-                        break;
-                    case 6:
-                        campo = (TextView)findViewById(R.id.est6);
-                        break;
-                    case 7:
-                        campo = (TextView)findViewById(R.id.est7);
-                        break;
-                    default:
-                        campo = null;
-                        break;
-                }
-                tiempo = Math.abs((Chrono.getBase()-SystemClock.elapsedRealtime())/1000);
-                minutos = (int)(tiempo/60);
-                segundos = (int)(tiempo-minutos*60);
-                string_min = String.valueOf(minutos);
-                string_seg = String.valueOf(segundos);
-                if (minutos < 10) {string_min = "0"+string_min;}
-                if (segundos < 10) {string_seg = "0"+string_seg;}
-                campo.setText(string_min+":"+string_seg);
-                Chrono.setBase(SystemClock.elapsedRealtime());
-                time = 0;
-                est++;
-                if (est > 7){ est = 1; }
+
                 break;
 
 
-        }
+        }*/
     }
     public void inicio(View view) {
         Intent i = new Intent(this, MainActivity.class );
